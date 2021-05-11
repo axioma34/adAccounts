9f5fc7da00671d058d19fda5520023ad6fdde6aa
rnt <andrew.rennit@gmail.com>
2021-05-11 18:30:55 +0700

    cors header fix

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
--- public/index.php
+++ public/index.php
@@ -5,6 +5,9 @@ use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
+header("Access-Control-Allow-Origin: http://localhost:8080");
+header('Access-Control-Allow-Credentials: true');
+
 require dirname(__DIR__).'/vendor/autoload.php';
 
 (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
